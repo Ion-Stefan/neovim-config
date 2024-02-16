8815688da0b231c0027ffc700dd056ba8ed2311a
Ion Stefan <ion.adrian.stefann@gmail.com>
2024-02-16 01:39:55 +0100

    neovim config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,22 @@
+-- Switch to the next buffer
+function Next_buffer()
+  vim.cmd("bnext")
+end
+-- Switch to the previous buffer
+function Prev_buffer()
+  vim.cmd("bprev")
+end
+
+-- Map tab to switch to the next buffer
+vim.api.nvim_set_keymap("n", "<Tab>", ":lua Next_buffer()<CR>", { noremap = true, silent = true })
+-- Map shift+tab to switch to the previous buffer
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":lua Prev_buffer()<CR>", { noremap = true, silent = true })
+
+-- Map ctrl+d and ctrl+u to also center the page when moving up or down
+vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>:normal! zz<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>:normal! zz<CR>", { noremap = true, silent = true })
+
+-- Keybinds for Trouble
+vim.keymap.set("n", "<leader>tt", function()
+  require("trouble").toggle()
+end)
---
+++ lua/plugins/ayu.lua
@@ -0,0 +1,12 @@
+return {
+
+  { "ayu-theme/ayu-vim" },
+  -- Configure LazyVim to load ayu
+  {
+    "LazyVim/LazyVim",
+
+    opts = {
+      colorscheme = "ayu",
+    },
+  },
+}
