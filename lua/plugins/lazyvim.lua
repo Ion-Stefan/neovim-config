90df9033b032fc670079ee74a1446779cefe9c2b
Ion Stefan <ion.adrian.stefann@gmail.com>
2024-06-10 02:36:23 +0200

    fix: copilot not attaching to new buffer

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -1,8 +1,22 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      autofromat = false,
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            autofromat = false,
+        },
+        {
+            "github/copilot.vim",
+            config = function()
+                vim.g.copilot_no_tab_map = true
+                vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+                vim.api.nvim_create_autocmd({ "BufEnter", "TabEnter" }, {
+                    callback = function()
+                        vim.cmd("Copilot enable")
+                    end,
+                })
+                vim.g.copilot_log_level = "debug"
+                vim.g.copilot_log_file = "/path/to/copilot.log" -- Change this to a valid path
+            end,
+        },
     },
-  },
 }
