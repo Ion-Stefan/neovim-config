418cdffd56d1e23cb981b42a53f39b442fdc924f
Ion Stefan <ion.adrian.stefann@gmail.com>
2024-11-02 02:08:26 +0100

    trouble, color, keymaps

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd [[highlight Normal guibg=NONE ctermbg=NONE]]
+vim.cmd [[highlight NonText guibg=NONE ctermbg=NONE]]
+vim.cmd [[highlight LineNr guibg=NONE ctermbg=NONE]]
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,24 +2,24 @@
 function Next_buffer()
   vim.cmd("bnext")
 end
+
 -- Switch to the previous buffer
 function Prev_buffer()
   vim.cmd("bprev")
 end
 
--- Map tab to switch to the next buffer
+-- Map Tab to switch to the next buffer
 vim.api.nvim_set_keymap("n", "<Tab>", ":lua Next_buffer()<CR>", { noremap = true, silent = true })
--- Map shift+tab to switch to the previous buffer
+
+-- Map Shift+Tab to switch to the previous buffer
 vim.api.nvim_set_keymap("n", "<S-Tab>", ":lua Prev_buffer()<CR>", { noremap = true, silent = true })
 
--- Map ctrl+d and ctrl+u to also center the page when moving up or down
-vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>:normal! zz<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>:normal! zz<CR>", { noremap = true, silent = true })
+-- Map Ctrl+D and Ctrl+U to also center the page when moving up or down
+vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
 
---yanking outside of neovim
-vim.api.nvim_set_keymap("n", "yy", [[:'<,'>w !xclip -selection clipboard<CR>yy]], { noremap = true })
+-- Enable yanking to the system clipboard
+vim.opt.clipboard:append("unnamedplus")
 
--- Keybinds for Trouble
-vim.keymap.set("n", "<leader>tt", function()
-  require("trouble").toggle()
-end)
+-- Yank selection to clipboard (uses visual mode for selection)
+vim.api.nvim_set_keymap('v', 'y', '"*y:!clip.exe<CR>', { noremap = true, silent = true })
